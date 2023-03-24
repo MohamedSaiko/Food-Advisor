02a03ba4d780f0739f027c07f5bb789e57afd5ef
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-03-24 20:38:28 +0200

    add home view model

--- Food Advisor/Home/HomeViewModel.swift
+++ Food Advisor/Home/HomeViewModel.swift
@@ -6,8 +6,34 @@
 //
 
 import Foundation
+import Combine
 
 final class HomeViewModel: ObservableObject {
     
+    @Published var posts: [Post] = []
     
+    private let networkManager = NetworkManager()
+    private var postssubscribers = Set<AnyCancellable>()
+    
+    func fetchPosts() {
+        
+        networkManager.getPosts()
+            .sink { result in
+                
+                switch result {
+                case .failure(let error):
+                    print(String(describing: error))
+                default:
+                    print("Completed!")
+                }
+                
+            } receiveValue: { [weak self] postsData in
+                guard let self = self else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.posts = postsData.posts
+                            }
+            }.store(in: &postssubscribers)
+    }
 }
