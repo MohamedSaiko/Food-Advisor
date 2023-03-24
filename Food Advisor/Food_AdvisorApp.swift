9e9ca4d482e36d37cdbf35f5a94de013415dd5f3
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-03-24 17:00:16 +0200

    add expandable search bar

--- Food Advisor/Food_AdvisorApp.swift
+++ Food Advisor/Food_AdvisorApp.swift
@@ -12,7 +12,8 @@ struct Food_AdvisorApp: App {
     var body: some Scene {
         WindowGroup {
             //ContentView()
-            HomeScreen()
+            //HomeScreen()
+            SignInScreen()
         }
     }
 }
