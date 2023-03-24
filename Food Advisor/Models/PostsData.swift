fdd7c93b3fc643665d7eff2cb28bff8394df0241
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-03-24 20:27:13 +0200

    add post data model and user model

---
+++ Food Advisor/Models/PostsData.swift
@@ -0,0 +1,21 @@
+//
+//  PostsData.swift
+//  Food Advisor
+//
+//  Created by Mohamed Sayed on 24.03.23.
+//
+
+import Foundation
+
+struct PostsData: Decodable {
+    
+    let posts: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    let id: Int
+    let title: String
+    let body: String
+    let userId: Int
+}
+
