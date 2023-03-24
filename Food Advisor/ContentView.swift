ab665427c0fb0d126ead631b4cded38cbb04cc30
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-03-24 06:21:50 +0200

    Initial Commit

---
+++ Food Advisor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Advisor
+//
+//  Created by Mohamed Sayed on 24.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food Advisor/Food_AdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AdvisorApp.swift
+//  Food Advisor
+//
+//  Created by Mohamed Sayed on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Food_AdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
