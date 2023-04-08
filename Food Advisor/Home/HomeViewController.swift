d72c93206f363afb5f8aac34008bdc59ab4f7530
MohamedSayed <Mohamedsaiko9@gmail.com>
2023-04-08 21:47:24 +0200

    add home screen

--- Food Advisor/Home/HomeViewController.swift
+++ Food Advisor/Home/HomeViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet private weak var postsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        postsTableView.delegate = self
+        postsTableView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Delegate
+
+extension HomeViewController: UITableViewDelegate {}
+
+// MARK: - Data Source
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let postsCell = tableView.dequeueReusableCell(withIdentifier: "postsCell", for: indexPath)
+        
+        return postsCell
     }
-    */
-
 }
